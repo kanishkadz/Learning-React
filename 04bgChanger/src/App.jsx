bc5375bc49fa8f642e1630a5bbff8bc7e7a15079
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-24 19:23:16 +0530

    Update App.jsx

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -8,7 +8,13 @@ function App() {
     duration-200" 
     style = {{backgroundColor: color}}
     >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">test</div>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"red"}}>Red</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"green"}}>Green</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"blue"}}>Blue</button>
+        </div>
+      </div>
     </div>
   )
 }
