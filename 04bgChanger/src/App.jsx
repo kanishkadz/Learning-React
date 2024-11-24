738c64b87ba9c75ccecc7bc564ea61617e88df52
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-24 19:24:44 +0530

    Update App.jsx

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -13,6 +13,11 @@ function App() {
           <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"red"}}>Red</button>
           <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"green"}}>Green</button>
           <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"blue"}}>Blue</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"yellow"}}>Yellow</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"pink"}}>Pink</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"orange"}}>Orange</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"white"}}>White</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor:"black"}}>Black</button>
         </div>
       </div>
     </div>
