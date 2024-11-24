ee76961c037e355a87446679c0ed018efbcdf3d0
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-24 15:45:04 +0530

    ok

--- 03taliwindprops/src/App.jsx
+++ 03taliwindprops/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='bg-green-400 text-black rounded'>Tailwind Test</h1>
+      <h1 className="bg-green-400 text-black rounded">Tailwind Test</h1>
+      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 rounded-full mx-auto"
+          src="/sarah-dayan.jpg"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. It’s easy to customize, adapts to any design, and the build size
+              is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="text-center">
+            <div>Sarah Dayan</div>
+            <div>Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
