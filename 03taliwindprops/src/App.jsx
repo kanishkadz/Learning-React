416be1d13795443362082644f2df2efdc4e11c7a
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-24 15:56:21 +0530

    props

--- 03taliwindprops/src/App.jsx
+++ 03taliwindprops/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Card from './components/Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1 className="bg-green-400 text-black rounded">Tailwind Test</h1>
+      <Card />
     </>
   );
 }
