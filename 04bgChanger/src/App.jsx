66f4b7480d47eb5699cec66eae66a493b64a13a6
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-24 19:18:13 +0530

    Update App.jsx

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <div className="w-full h-screen
-    duration-200"></div>
+    duration-200" 
+    style = {{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0"></div>
+    </div>
   )
 }
 
