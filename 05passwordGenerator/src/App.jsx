c7f4123db857b38e498eb8c38e9b889f799b7583
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-25 12:47:43 +0530

    Update App.jsx

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>
-        <h1 className='text-white text-center'>Password Generator</h1>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb4'>
           <input type="text" 
           value={password}
@@ -35,6 +35,19 @@ function App() {
           placeholder='password'
           readOnly
           />
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        </div>
+        <div className='flex tex-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="range" 
+            min={8} 
+            max= {100}
+            value={length}
+            className='cursor-pointer'
+            />
+            <label>Length: {length}</label>
+          </div>
         </div>
       </div>
     </>
