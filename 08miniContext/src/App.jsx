d780880f1fa69177640426f4641d39fb1cbd8ccf
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-26 22:16:04 +0530

    Update App.jsx

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile' 
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
