81bfd3e39458fb6025ccb016ac0075b01d115a4e
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-26 18:55:48 +0530

    Update main.jsx

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
@@ -27,6 +27,7 @@ const router = createBrowserRouter([
     ]
   }
 ])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
