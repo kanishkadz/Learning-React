883174b076b44141cdd1b48b4a543e855885a869
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-26 22:50:59 +0530

    Update App.jsx

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ThemeProvider>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
