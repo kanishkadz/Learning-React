ce90dd2b72ef6f60b72d3fb9f21085679ad47cb8
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-26 22:45:40 +0530

    Update theme.js

--- 09themeSwitcher/src/contexts/theme.js
+++ 09themeSwitcher/src/contexts/theme.js
@@ -1,9 +1,13 @@
 import { createContext, useContext } from "react";
 
-
-
 export default ThemeContext = createContext({
     themeMode: "light",
     darkTheme: () => {},
     lightTheme: () => {},
 })
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
