949c6add65c2983d368c019ddf5167a479faa5ad
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-26 18:38:34 +0530

    07reactRouter

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function Layout() {
+  return (
+    <>
+    <Header />
+    
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
