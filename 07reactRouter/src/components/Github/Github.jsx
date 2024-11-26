2435e42f978e2c89eb54d7ba003af94abce707d1
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-26 19:08:34 +0530

    Update Github.jsx

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/users/kanishkadz')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    }, [])
   return (
     <div className='text-center m-4 bg-grey-600 text-white text-3xl'>
         Github Followers:
