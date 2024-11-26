8b44dd6021bb5502bd911ebe4b2a07d03cee6c60
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-26 18:48:39 +0530

    Update Layout.jsx

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
@@ -6,7 +7,7 @@ function Layout() {
   return (
     <>
     <Header />
-    
+    <Outlet />
     <Footer />
     </>
   )
