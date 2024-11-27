07e83dd2f7590842bc510099a76f980a04cc7075
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-27 11:17:35 +0530

    Update App.jsx

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import './App.css';
-import {TodoProvider} from './contexts';
+import { TodoProvider } from './contexts';
 
 function App() {
   const [todos, setTodods] = useState([])
 
   return (
-    <TodoProvider>
+    <TodoProvider value={{todos, addTodo, updatedTodo, deleteTodo, toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">
