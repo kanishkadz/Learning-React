55a7c9f04206ba498730fc3984e7c5bb8f5e4aa1
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-27 11:03:29 +0530

    Update TodoContext.js

--- 10todocontextLocal/src/context/TodoContext.js
+++ 10todocontextLocal/src/context/TodoContext.js
@@ -1,6 +1,14 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todos : [
+        {
+            id: 1,
+            todo: "Todo msg",
+            completed: false,
+        }
+    ]
+})
 
 export const useTodo = () => {
     return useContext(TodoContext)
